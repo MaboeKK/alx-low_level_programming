fb45403a1c55b1a3e1e75ae81d293c550e961f84
Keiketlile <maboekeiketlile@gmail.com>
2022-10-22 11:34:53 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - Multiplying
+ * @num1: First int
+ * @num2: Second int
+ * Return: First int multiplied by Second int
+ */
+int mul(int num1, num2)
+{
+	return (num1 * num2);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,7 +3,7 @@
 
 int _isupper(int c);
 int _isdigit(int dgt);
-int mul(int a, int b);
+int mul(int num1, int num2);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
