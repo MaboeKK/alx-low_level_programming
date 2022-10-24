e3d6e9cb26c85647cbe359693cbe716c895c4a17
Keiketlile <maboekeiketlile@gmail.com>
2022-10-24 16:59:17 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 
 /**
- * quote - Entry point
+ * main - Entry point
  * Return: 1
  */
-int quote(void)
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
