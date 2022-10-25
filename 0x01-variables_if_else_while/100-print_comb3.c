5740ca089dce000a56da577f8b447881f66eb8fd
Keiketlile <maboekeiketlile@gmail.com>
2022-10-25 08:02:51 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,19 +12,19 @@ int main(void)
 
 	for (tens = '0'; tens <= '9'; tens++)
 	{
-		for (ones = '0'; ones <= '9'; ones++)
+	for (ones = '0'; ones <= '9'; ones++)
+	{
+		if (!((ones == tens) || (tens < ones)))
 		{
-			if (!((ones == tens) || (tens < ones)))
+			putchar(tens);
+			putchar(ones);
+			if (!(ones == '9' && tens == '8'))
 			{
-				putchar(tens);
-				putchar(ones);
-				if (!(ones == '9' && tens == '8'))
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
 			}
 		}
+	}
 	}
 
 	putchar('\n');
