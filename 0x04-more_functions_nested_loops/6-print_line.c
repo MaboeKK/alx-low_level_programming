f7ae022bfa88d0af3ddd9e947df4846d6bde632f
MaboeKK <maboekeiketlile@gmail.com>
2023-02-23 11:23:42 -0800

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,14 @@
 #include "main.h"
-
 /**
- * print_line - Prints A Strasight Line Using '_'
- * @n: The Number of '_' To Be Printed
- */
+*print_line - prints a straight line
+*@n:parameter
+*Return:returns nothing
+*/
 void print_line(int n)
 {
-	int len;
-
-	if (n > 0)
+	while (n-- > 0)
 	{
-		for (len = 0; len < n; len++)
-			_putchar('_');
+		_putchar('_');
 	}
 	_putchar('\n');
-}	
+}
