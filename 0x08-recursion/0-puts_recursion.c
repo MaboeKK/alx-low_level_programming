dea90af1e7d8aa884ef0c749708f8e8637d18505
MaboeKK <maboekeiketlile@gmail.com>
2023-03-14 05:54:09 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,16 +2,17 @@
 
 /**
  * _puts_recursion - Prints string then adds new line
- * @s: string variable
- * Return: 1
- * Exeption, on error -1 is returned.
+ * @s: string variable.
+ *
+ * Return: nothing.
  */
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1);
+		s++;
+		_puts_recursion(s);
 	}
 	else
 		_putchar('\n');
