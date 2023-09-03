72944955cbd505dbb8510cce27e7d4785998e9c8
MaboeKK <maboekeiketlile@gmail.com>
2023-09-03 18:13:23 +0200

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,7 +5,6 @@
  * @x: Input number
  * @y: Power number
  *
- * Return: if "y" is bigger than zero raised to the power of "y".
  * Return: -1 if "y" is lower than zero, return 1 if "y" is equal to 1.
  */
 int _pow_recursion(int x, int y)
