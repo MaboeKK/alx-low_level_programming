5d00954f36bf86263b4d2bb7895bc25605c514f3
MaboeKK <maboekeiketlile@gmail.com>
2023-09-05 00:54:17 +0200

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,22 +7,19 @@
  * Return: pointer to array, NULL if fails
  */
 
-char *create_array(usigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
-	int i;
+	unsigned int i;
 	char *arr;
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
+
 	arr = malloc(sizeof(char) * size);
 
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-	{
 		arr[i] = c;
-	}
 	return (arr);
 }
