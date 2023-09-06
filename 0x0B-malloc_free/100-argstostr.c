71ee8883f4ea7e4b0a8a772e1e36f9bd47f3f11b
MaboeKK <maboekeiketlile@gmail.com>
2023-09-06 03:17:29 +0200

    Task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * argstostr - Concatenates all args of the program.
+ * @ac: args count.
+ * @av: args vector.
+ * Return: NULL.
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, a, g = 0, count = 0;
+	char *outp;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (a = 0; av[i][a] != '\0'; a++)
+			count++;
+		count++;
+	}
+	outp = malloc(sizeof(char) * count + 1);
+	if (outp == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (a = 0; av[i][a] != '\0'; a++)
+		{
+			outp[g] = av[i][a];
+			g++;
+		}
+		outp[g++] = '\n';
+	}
+	outp[g] = '\0';
+	return (outp);
+}
+
