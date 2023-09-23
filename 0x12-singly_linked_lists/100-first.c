6ed06ce4a61795b820d796e6b954c1ddf91116ed
MaboeKK <maboekeiketlile@gmail.com>
2023-09-23 11:30:51 +0200

    Advanced Task 0

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include "stdio.h"
+
+/**
+ * first - To print words before main function.
+ * Return: Words.
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!");
+}
+
