c4e0338fb1adc2bd780b774b6e0a8fe068848071
MaboeKK <maboekeiketlile@gmail.com>
2023-10-03 11:45:03 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 
 	arr = malloc(sizeof(char) * size);
 
-	if (arr == NUUL)
+	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		arr[i] = c;
